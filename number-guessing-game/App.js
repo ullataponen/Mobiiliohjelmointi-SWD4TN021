e9e83b880bf01b6c36cf26abb52b26762860fb09
ullataponen <ulla.taponen@gmail.com>
2020-04-12 18:30:43 +0200

    Exercise 6

--- number-guessing-game/App.js
+++ number-guessing-game/App.js
@@ -1,5 +1,14 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
+import React, { useState, useEffect } from "react";
+
+import {
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	Button,
+	Alert,
+	AsyncStorage,
+} from "react-native";
 
 export default function App() {
 	const [text, setText] = useState("Guess a number between 1-100");
@@ -8,17 +17,22 @@ export default function App() {
 	const [randomNumber, setRandomNumber] = useState(
 		Math.floor(Math.random() * 100) + 1
 	);
+	let top;
 
 	const checkMatch = () => {
 		setCounter(counter + 1);
+		console.log(counter);
 		if (value < randomNumber) {
-			setText(`Your guess ${value} is smaller than the number`);
+			setText(`Your guess ${value} is too small`);
 		} else if (value > randomNumber) {
-			setText(`Your guess ${value} is bigger than the number`);
+			setText(`Your guess ${value} is too big`);
 		} else {
 			Alert.alert(
 				`Correct number! You guessed the number in ${counter} guesses.`
 			);
+			checkIfTop(counter);
+			top = getHighScore().then((result) => result.data);
+			console.log("top,", top, typeof top);
 			setRandomNumber(Math.floor(Math.random() * 100) + 1);
 			setCounter(1);
 			setText("Guess a number between 1-100");
@@ -27,20 +41,53 @@ export default function App() {
 		return text;
 	};
 
+	const checkIfTop = async (counter) => {
+		console.log("this is top", top, typeof top);
+		if (top === undefined || top === NaN || counter < top) {
+			await setHighScore(counter);
+		}
+	};
+
+	setHighScore = async (val) => {
+		console.log("Now sethighscore runs", val, typeof val);
+		try {
+			let item = await AsyncStorage.setItem("HighScore", JSON.stringify(val));
+			console.log("item", item, typeof item);
+			return item;
+		} catch (e) {
+			Alert.alert("Error saving data");
+		}
+	};
+
+	getHighScore = async () => {
+		try {
+			const value = await AsyncStorage.getItem("HighScore");
+			const jsonVal = JSON.parse(value);
+			console.log("this is gethiscore", jsonVal, typeof jsonVal);
+			return jsonVal;
+		} catch (error) {
+			Alert.alert("Error reading data");
+		}
+	};
+
 	return (
 		<View style={styles.container}>
 			<Text>{text}</Text>
 			<TextInput
+				keyboardType="numeric"
 				style={{
 					width: 100,
 					borderColor: "lightgray",
 					borderWidth: 1,
-					margin: 10
+					margin: 10,
 				}}
-				onChangeText={value => setValue(value)}
+				onChangeText={(value) => setValue(value)}
 				value={value}
 			/>
 			<Button title="Make a Guess" onPress={checkMatch} />
+			<Text style={{ marginTop: 20 }}>
+				Highscore: {top ? `${top} guesses` : "No guesses"}
+			</Text>
 		</View>
 	);
 }
@@ -50,6 +97,6 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: "#fff",
 		alignItems: "center",
-		justifyContent: "center"
-	}
+		justifyContent: "center",
+	},
 });
